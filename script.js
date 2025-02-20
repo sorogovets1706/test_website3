3ae62597b18167d0d83cae34e12b75886119e42b
sorogovets1706 <119353891+sorogovets1706@users.noreply.github.com>
2025-02-20 14:11:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function submitData() {
     // Получаем значения из полей ввода
-    const c1 = document.getElementById('c1').value;
-    const c2 = document.getElementById('c2').value;
-    const c3 = document.getElementById('c3').value;
-
-    // Формируем строку для вывода
+    const c1 = document.getElementById('c1').value; const c2 = document.getElementById('c2').value; const c3 = document.getElementById('c3').value;
     const outputString = `${c1}|${c2}|${c3}`;
 
     // Выводим результат на страницу
